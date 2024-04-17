f628e244f2a9f9d0b35250ec73b53cbdcb0b5a48
yhkim8046 <yhkim8046@gmail.com>
2024-04-17 18:56:04 +1200

    Updating api added

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -45,6 +45,12 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
+    @Transactional
+    public void update(Long id, String name){
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
+
     /*
         readOnly allows to database optimise itself
         Annotation Transactional has true as default for readOnly
